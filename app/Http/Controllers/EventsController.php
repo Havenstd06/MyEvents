f7d75cc13b123f770586f32b7e088cef97ec82ea
Thomas <me@hvs.cx>
2021-08-31 16:39:39 +0200

    remove http event api request from back

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -12,10 +12,7 @@ class EventsController extends Controller
 
     public function home()
     {
-        $trendingEvents = Http::get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-cibul&q=&lang=fr&rows=18&facet=tags&facet=placename&facet=department&facet=region&facet=city&facet=date_start&facet=date_end&facet=pricing_info&facet=updated_at&facet=city_district&refine.city=Marseille&timezone=Europe%2FParis')
-            ->json()['records'];
-
-        return Inertia::render('Home', compact('trendingEvents'));
+        return Inertia::render('Home');
     }
 
     /**
@@ -24,9 +21,6 @@ class EventsController extends Controller
      */
     public function show(Request $request, $recordid)
     {
-        $event = Http::get("https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-cibul&q=&lang=fr&rows=18&facet=tags&facet=placename&facet=department&facet=region&facet=city&facet=date_start&facet=date_end&facet=pricing_info&facet=updated_at&facet=city_district&facet=recordid&refine.city=Marseille&refine.recordid=$recordid&timezone=Europe%2FParis")
-            ->json()['records'][0];
-
-        return Inertia::render('Events/Show', compact('event'));
+        return Inertia::render('Events/Show', compact('recordid'));
     }
 }
