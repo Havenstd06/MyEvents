7f30c14c0fe5a95816c6263e375af491ee80c5ab
Thomas <me@hvs.cx>
2021-09-04 18:05:54 +0200

    Display number of participants on profile page

--- resources/js/Pages/Users/Profile.vue
+++ resources/js/Pages/Users/Profile.vue
@@ -7,7 +7,7 @@
                     <!-- Profile Card -->
                     <div class="bg-blueGray-700 p-3 border-t-4 border-blueGray-400">
                         <div class="image overflow-hidden">
-                            <img class="h-auto w-full mx-auto" :src="'/'+user.avatar" alt="">
+                            <img class="h-auto w-full mx-auto" :src="'/' + user.avatar" :alt="user.name + '\'s avatar'">
                         </div>
                         <h1 class="text-gray-100 font-bold text-xl leading-8 my-1" v-html="user.name" />
                         <p class="text-sm text-gray-300 leading-6" v-html="user.description ?? 'No description'" />
@@ -24,13 +24,22 @@
                                Organized Trips
                             </span>
                         </div>
-                        <div v-if="this.userOrganizedTrips.length > 0" class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3">
+                        <div v-if="userOrganizedTrips.length > 0"
+                             class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3"
+                        >
                             <a v-for="trip in userOrganizedTrips"
                                :key="trip.id"
                                :href="route('trips.show', {'trip_id': trip.id})"
-                               class="bg-blueGray-800 px-2 py-1.5 rounded-md group">
-                                <div class="text-blueGray-200 group-hover:text-gray-400 transition duration-200 ease-in-out" v-html="trip.name" />
-                                <div class="text-gray-300 text-xs group-hover:text-gray-400 transition duration-200 ease-in-out" v-html="user.name" />
+                               class="bg-blueGray-800 hover:bg-blueGray-900 transition duration-200 ease-in-out px-2 py-1.5 rounded-md group">
+                                <div class="flex items-center justify-between space-x-4 group">
+                                    <div>
+                                        <div class="text-blueGray-200 group-hover:text-gray-400 transition duration-200 ease-in-out" v-html="trip.name" />
+                                        <div class="text-blueGray-300 text-xs group-hover:text-gray-400 transition duration-200 ease-in-out" >
+                                            by <span v-html="trip.user.name" />
+                                        </div>
+                                    </div>
+                                    <div class="text-xs text-blueGray-300 group-hover:text-gray-400" v-html="(trip.participants.length ?? 'x') + ' participants'" />
+                                </div>
                             </a>
                         </div>
                         <div v-else>
@@ -48,13 +57,22 @@
                                Joined Trips
                             </span>
                         </div>
-                        <div v-if="this.userJoinedTrips.length > 0" class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3">
+                        <div v-if="userJoinedTrips.length > 0"
+                             class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3"
+                        >
                             <a v-for="trip in userJoinedTrips"
                                :key="trip.id"
                                :href="route('trips.show', {'trip_id': trip.id})"
-                               class="bg-blueGray-800 px-2 py-1.5 rounded-md group">
-                                <div class="text-blueGray-200 group-hover:text-gray-400 transition duration-200 ease-in-out" v-html="trip.name" />
-                                <div class="text-gray-300 text-xs group-hover:text-gray-400 transition duration-200 ease-in-out" v-html="trip.user.name" />
+                               class="bg-blueGray-800 hover:bg-blueGray-900 transition duration-200 ease-in-out px-2 py-1.5 rounded-md">
+                                <div class="flex items-center justify-between space-x-4 group">
+                                    <div>
+                                        <div class="text-blueGray-200 group-hover:text-gray-400 transition duration-200 ease-in-out" v-html="trip.name" />
+                                        <div class="text-blueGray-300 text-xs group-hover:text-gray-400 transition duration-200 ease-in-out" >
+                                            by <span v-html="trip.user.name" />
+                                        </div>
+                                    </div>
+                                    <div class="text-xs text-blueGray-300 group-hover:text-gray-400" v-html="(trip.participants.length ?? 'x') + ' participants'" />
+                                </div>
                             </a>
                         </div>
                         <div v-else>
