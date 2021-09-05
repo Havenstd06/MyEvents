c654f6b18ea21ff9cde63d9a4fbb0ff8a77c083b
Thomas <me@hvs.cx>
2021-09-06 01:33:35 +0200

    Loading when send message

--- resources/js/Pages/Trips/Partials/Chat/InputBox.vue
+++ resources/js/Pages/Trips/Partials/Chat/InputBox.vue
@@ -18,7 +18,19 @@
                     class="flex items-center justify-center bg-blueGray-500 hover:bg-blueGray-600 rounded-md text-white px-4 py-1.5 flex-shrink-0"
                 >
                     <span>Send</span>
-                    <span class="ml-2">
+
+                    <span v-if="loading"
+                          class="ml-2"
+                    >
+                         <svg class="animate-spin h-4 w-4 -mt-2 -mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                    </span>
+
+                    <span v-else
+                          class="ml-2"
+                    >
                   <svg class="w-4 h-4 transform rotate-45 -mt-px -mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
                   </svg>
@@ -34,7 +46,8 @@ export default {
   props: ['room'],
   data () {
     return {
-      message: ''
+      message: '',
+      loading: false
     }
   },
   methods: {
@@ -46,17 +59,19 @@ export default {
       if (!this.message) {
         return
       }
-      axios
-        .post(`/trips/chat/${this.room}`, {
-          text: this.message
-        })
-        .then(response => {
+        var newMessage = this.message
+      this.message = ''
+      this.loading = true
+
+        axios.post(`/trips/chat/${this.room}`, {
+          text: newMessage
+        }).then(response => {
           if (response.status === 201) {
-            this.message = ''
-            this.$emit('messageSent')
+              newMessage = ''
+              this.loading = false
+              this.$emit('messageSent')
           }
-        })
-        .catch(error => console.log(error.message))
+        }).catch(error => console.log(error.message))
     }
   }
 }
